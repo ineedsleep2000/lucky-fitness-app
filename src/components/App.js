5a4c6eac7bce8ca0ecdfc52773d396f505a3cb14
Jessica <jess.pichardo18@gmail.com>
2024-05-24 22:27:18 -0700

    updated header

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import logo from "../logo.svg";
 import "../App.css";
 import Contact from "./Contact";
 import About from "./About";
-import Home from "./Home";
+import Home from "./Header";
 
 function App() {
   return (
