2a94d9fc95bd0f42bd4400acdab267e9cb8d8e9b
Jessica <jess.pichardo18@gmail.com>
2024-05-25 16:08:23 -0700

    edits to header and page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,13 @@
 import React from "react";
-
+import logo from '../assets/lucky-fitness.png'
 const Header = () => {
   return (
-    <div className="headerbackground">
-      <img className="logo" src="https://cdn.discordapp.com/attachments/1232074071941054564/1243697332009304105/Lucky_Fitness_Logo.png?ex=66526ae6&is=66511966&hm=4045fbd3c0306ae8ab39573212e0eddfad9fab4e74f65eb3f271a9f51713dc38&" alt="luckyfitness" />
-     
+    <div className="navbar">
+
+      <img src={logo} alt="luckyfitness" className="logo" />
+    
      <ul>
+      <li className="title">LUCKY FITNESS</li>
       <li>About</li>
       <li>Contact Us</li>
      </ul>
