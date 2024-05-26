98a6cc6f0d943a233e796ece48a3c2ea09fd03d4
Jessica <jess.pichardo18@gmail.com>
2024-05-26 03:57:56 -0700

    css edits

--- src/components/WorkoutCard.js
+++ src/components/WorkoutCard.js
@@ -1,4 +1,6 @@
 import React from "react";
+import "../WorkoutCard.css";
+
 
 const WorkoutCard = ({ workout, onDeleteWorkout }) => {
   const { id, name, image } = workout;
@@ -7,30 +9,28 @@ const WorkoutCard = ({ workout, onDeleteWorkout }) => {
     console.log("Initiating delete for workout ID:", id);
     fetch(`http://localhost:6001/workouts/${id}`, {
       method: "DELETE",
-    })
-      .then((response) => {
-        if (response.ok) {
-          console.log("Deleted workout with ID:", id);
-          onDeleteWorkout(id);
-        } else {
-          console.error(
-            "Failed to delete workout with ID:",
-            id,
-            "Status:",
-            response.status
-          );
-        }
-      })
-      .catch((error) => console.error("Error:", error));
+    }).then((response) => {
+      if (response.ok) {
+        console.log("Deleted workout with ID:", id);
+        onDeleteWorkout(id); // Update parent component state after successful deletion
+      }
+    });
   };
 
   return (
-    <li className="workoutCards">
-      <img src={image} alt={name} />
-      <h4>{name}</h4>
-      <button onClick={handleDelete}>Delete</button>
-    </li>
+    <div className="workout-card">
+      
+      <img src={image} alt={name} className="workout-image"/>
+
+        <h3 className="workout-name">{name}</h3>
+
+        <button className="delete-button" onClick={handleDelete}>Delete</button>
+
+        </div>
   );
 };
 
 export default WorkoutCard;
+
+
+
