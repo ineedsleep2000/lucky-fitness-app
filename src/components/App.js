08bbb2b1a99aea10f078e0f9f9d9d858917de50a
Brian <turtle40965@gmail.com>
2024-05-28 13:57:39 -0400

    got WelcomePage to work

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,6 @@ import Contact from "./Contact";
 import About from "./About";
 import Header from "./Header";
 import WorkoutPage from "./WorkoutPage";
-import AddWorkout from "./AddWorkout";
 import WelcomePage from "./WelcomePage";
 
 const App = () => {
@@ -13,10 +12,10 @@ const App = () => {
       <div className="container">
         <Header />
         <Routes>
+          <Route path="" element={<WelcomePage />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/workoutpage" element={<WorkoutPage />} />
-          <Route path="/add-workout-to-page" element={<AddWorkout />} />
         </Routes>
       </div>
     </Router>
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,31 +2,28 @@ import React from "react";
 import { Link } from "react-router-dom";
 import logo from "../assets/lucky-fitness.png";
 const Header = () => {
-
   return (
-    
     <div className="navbar">
+      <img src={logo} alt="luckyfitness" className="logo" />
 
-        <img src={logo} alt="luckyfitness" className="logo" />
-        
-        <ul className="navigation">
-          <li>
-            <Link className="button" to="/workoutpage">
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link className="button" to="/contact">
-              Contact Us
-            </Link>
-          </li>
-          <li>
-            <Link className="button" to="/about">
-              About
-            </Link>
-          </li>
-        </ul>
-      </div>
+      <ul className="navigation">
+        <li>
+          <Link className="button" to="/workoutpage">
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link className="button" to="/contact">
+            Contact Us
+          </Link>
+        </li>
+        <li>
+          <Link className="button" to="/about">
+            About
+          </Link>
+        </li>
+      </ul>
+    </div>
   );
 };
 
